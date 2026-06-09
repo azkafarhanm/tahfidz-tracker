389de595a1666063f118aaa3f7c7537a05ecdc01
azkafarhanm <azkafarhanm@gmail.com>
2026-06-09 13:13:51 +0700

    NEW updated 4

--- web/src/i18n/actions.ts
+++ web/src/i18n/actions.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { cookies } from "next/headers";
+import { revalidatePath } from "next/cache";
 
 const VALID_LOCALES = new Set(["id", "en", "ar"]);
 
@@ -8,4 +9,5 @@ export async function setLocale(locale: string) {
   if (!VALID_LOCALES.has(locale)) return;
   const store = await cookies();
   store.set("locale", locale, { path: "/", maxAge: 60 * 60 * 24 * 365 });
+  revalidatePath("/");
 }
